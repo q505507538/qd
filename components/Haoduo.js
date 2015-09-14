d4cee5964d8c8b8a4315bee01f3962adc29f2985
q505507538 <505507538@qq.com>
2015-09-14 13:41:47 +0800

    webpack sass

--- components/Haoduo.js
+++ components/Haoduo.js
@@ -4,7 +4,7 @@ var Haoduo = React.createClass({
     return (
       <div className="haoduo clearfix">
         <h2>好多视频网</h2>
-        <p>happypeter web 开发技巧分享</p>
+        <p>Happypeter web 开发技巧分享</p>
         <a href="#" className="visit-haoduo">访问</a>
       </div>
     );
--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 var App = require('./components/App.js');
 var React = require('react');
+require('./styles/main.scss');
 React.render(<App />, document.getElementById('app'));
